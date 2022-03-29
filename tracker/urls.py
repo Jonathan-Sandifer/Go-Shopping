5e68ca4cb3efd6ecd20e97a02e0f91a4ac2de96b
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-03-29 13:44:51 -0500

    Feature 5 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("projects/", include("projects.urls")),
+    # path("tasks/", include("tasks.urls")),
+    # path("accounts/", include("accounts.urls")),
+    path("", RedirectView.as_view(url=reverse_lazy("home"))),
 ]
