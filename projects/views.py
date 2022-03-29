7fbf74c3def5a645d720b7162624d671dba795b8
Jonathan Sandifer <jaysandifer5000@gmail.com>
2022-03-28 21:25:07 -0500

    Feature 5 not done yet

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from projects.models import Project
+from django.views.generic.list import ListView
 # Create your views here.
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "projects/list.html"
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
